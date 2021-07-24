faef901eb3d357fa80a237db3143ddb15d308a73
Igor Kozyrenko <igor@ikseek.com>
2021-07-24 09:32:05 +0300

    Lists are moved to https://www.six-group.com

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -7,6 +7,9 @@ from iso_4217.lists import _load_xml_resource
 
 @pytest.mark.parametrize("list_xml", ("list_one.xml", "list_three.xml"))
 def test_data_tables_are_up_to_date(list_xml):
-    LISTS_URL = "http://www.currency-iso.org/dam/downloads/lists/"
+    LISTS_URL = (
+        "https://www.six-group.com/dam/download/financial-information/"
+        "data-center/iso-currrency/amendments/lists/"
+    )
 
     assert _load_xml_resource(list_xml) == urlopen(LISTS_URL + list_xml).read()
