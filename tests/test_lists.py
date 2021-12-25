20f28e4ab882ae11f610474ce8031593903fe868
Igor Kozyrenko <igor@ikseek.com>
2021-12-25 07:19:22 +0200

    Update lists

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -9,7 +9,7 @@ from iso_4217.lists import _load_xml_resource
 def test_data_tables_are_up_to_date(list_xml):
     LISTS_URL = (
         "https://www.six-group.com/dam/download/financial-information/"
-        "data-center/iso-currrency/amendments/lists/"
+        "data-center/iso-currrency/lists/"
     )
 
     assert _load_xml_resource(list_xml) == urlopen(LISTS_URL + list_xml).read()
